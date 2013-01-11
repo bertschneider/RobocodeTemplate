0d2bbda75cb6a0ccab716a9826b9504d6c09a5e0
Norbert Schneider <norbert.schneider@opitz-consulting.com>
2013-01-11 09:58:20 +0100

    Initial commit.
    Added the Robocode template and Maven build file.

---
+++ src/main/java/de/oc/robocode/FirstRobot.java
@@ -0,0 +1,31 @@
+package de.oc.robocode;
+
+import robocode.HitByBulletEvent;
+import robocode.Robot;
+import robocode.ScannedRobotEvent;
+
+public class FirstRobot extends Robot {
+
+    @Override
+    public void run() {
+
+        double radius = 100.0;
+        double angle = 90.0;
+
+        while (true) {
+            ahead(radius);
+            turnLeft(angle);
+            turnGunLeft(angle);
+            fireBullet(getEnergy());
+        }
+    }
+
+    public void onScannedRobot(ScannedRobotEvent e) {
+        fire(1);
+    }
+
+    public void onHitByBullet(HitByBulletEvent e) {
+        turnLeft(90 - e.getBearing());
+    }
+
+}
